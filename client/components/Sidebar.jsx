38c1fb0893e438f7cc18279cf4a8522f0efb8bbe
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-03-06 01:16:39 +0530

    adjusted sidebar width

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import {MdKeyboardArrowRight} from 'react-icons/md'
 const Sidebar = ({setShowSidebar}) => {
   return (
     <div onClick={()=>setShowSidebar(false)} className='bg-[rgba(0,0,0,0.7)] h-screen w-screen fixed top-0 left-0 z-40'>
-    <div onClick={(e)=>e.stopPropagation()} className='h-screen  w-[80vw] fixed z-50 left-0 top-0 p-3 bg-white'>
+    <div onClick={(e)=>e.stopPropagation()} className='h-screen  w-[75vw] fixed z-50 left-0 top-0 p-3 bg-white'>
         <div className='flex justify-between'>
         <div className='mt-3 flex space-x-2'>
             <div className='border-[1px] rounded-full p-2 w-[60px] h-[60px] flex justify-center items-center'>
