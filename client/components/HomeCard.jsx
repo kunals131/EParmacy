56822b9a6806f25bf4ab16abbdda14c6d5f0b776
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-03-12 21:52:46 +0530

    Fixed Home page responsiveness

--- client/components/HomeCard.jsx
+++ client/components/HomeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image';
 const HomeCard = () => {
   return (
-    <div className='bg-white w-[calc((100vw-3.75rem-11rem)/5)] h-[300px] flex flex-col justify-center items-center rounded-xl shadow-lg'>
+    <div className='bg-white w-[calc((100vw-3.75rem-11rem)/3)] xl:w-[calc((100vw-3.75rem-11rem)/4)] 2xl:w-[calc((100vw-3.75rem-11rem)/5)] h-[300px] flex flex-col justify-center items-center rounded-xl shadow-lg'>
         <div className=''>
             <Image src="/categoryImage/pic1.png" height="180px" width="180px" ></Image>
         </div>
--- client/components/ProductCard/ProductCard.jsx
+++ client/components/ProductCard/ProductCard.jsx
@@ -4,7 +4,7 @@ import {MdShoppingCart} from 'react-icons/md'
 const ProductCard = () => {
     const [counter,setCounter] = useState(0);
   return (
-    <div className='py-3 px-2 bg-white w-[calc((100vw-1rem)/2)] sm:w-[calc((100vw-2rem)/3)] md:w-[calc((100vw-1rem)/3)]  lg:w-[calc((100vw-3.75rem-11rem)/6)] border-[1px] border-gray-300 rounded-lg'>
+    <div className='py-3 px-2 bg-white w-[calc((100vw-1rem)/2)] sm:w-[calc((100vw-2rem)/3)] md:w-[calc((100vw-1rem)/3)]  lg:w-[calc((100vw-3.75rem-11rem)/4)] 2xl:w-[calc((100vw-3.75rem-11rem)/5)] 3xl:w-[calc((100vw-3.75rem-11rem)/6)] border-[1px] border-gray-300 rounded-lg'>
         <div className='flex justify-end relative -top-3 left-2 '>
             <div className='flex justify-center text-xs p-1 items-center text-white bg-[#37b9c596] w-[40%]'>10% OFF</div>
         </div>
