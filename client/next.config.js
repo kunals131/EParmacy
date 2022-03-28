617fcb1447b119f7ccda4fb49bb3d337b662e1fe
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-03-28 13:28:12 +0530

    Next config to support test files

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['img6a.flixcart.com','img5a.flixcart.com' ],
+  },
 }
 
 module.exports = nextConfig
